466142b94d628e421e1858fade3b2869929a58c7
mahmoud ahmed <mahmoudahmedhsn@gmail>
2025-03-03 23:33:57 +0200

    finsh ui

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:payment/Features/checkout/presentation/views/my_cart_view.dart';
+
+void main() {
+  runApp(const PaymentApp());
+}
+
+class PaymentApp extends StatelessWidget {
+  const PaymentApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyCartView(),
+    );
+  }
+}
